3b7d16d901f000f53edb63b45139ac3798ecb8ae
Iury-AB <iuryalves.iab@gmail.com>
2025-10-24 19:28:29 -0300

    funcoes de restricoes

---
+++ implementacao/Restricoes.py
@@ -0,0 +1,35 @@
+from exemplo_prof.dados import Dados
+from exemplo_prof.solucao import Solucao
+
+def atendimento_requisicoes(solucao: Solucao):
+
+  return True or False
+
+def conservacao_de_fluxo(solucao: Solucao):
+
+  return True or False
+
+def inicio_e_fim_de_cada_viagem(solucao: Solucao):
+
+  return True or False
+
+def sequencia_de_viagens(solucao: Solucao):
+
+  return True or False
+
+def janela_de_tempo_da_coleta(solucao: Solucao):
+
+  return True or False
+
+def Sequencia_temporal_das_rotas_inter(solucao: Solucao):
+
+  return True or False
+
+def sequencia_temporal_das_rotas_intra(solucao: Solucao):
+
+  return True or False
+
+def limite_de_tempo_por_viagem(solucao: Solucao):
+
+  return True or False
+
