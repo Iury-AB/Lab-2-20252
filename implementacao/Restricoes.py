0d6879bdd3d52f2683f18266ccd1841e8f6a5239
Paulo Santos <paulormsantos@gmail.com>
2025-11-02 09:08:33 -0300

    Corrigido vários métodos das restrições. Agora no primeiro não atendimento a elas, false é retornado

--- implementacao/Restricoes.py
+++ implementacao/Restricoes.py
@@ -4,15 +4,22 @@ from exemplo_prof.solucao import Solucao
 def atendimento_requisicoes(solucao: Solucao, dados: Dados):
   # cada requisição deve ser atendida exatamente uma vez
   # comentario a mais
+  req_atendidas = []
   for k, viagens in solucao.rota.items():
     for v, rota in viagens.items():
         if not rota or len(rota) <=1:
             continue  # Pula viagens vazias ou sem paradas intermediárias
-        req_atendidas = []
+        
         for req in rota[1:-1]:  # Ignora a garagem no início e no fim
           req_atendidas.append(req)
-        if len(req_atendidas) > dados.n:
-            return False
+  if len(req_atendidas) != dados.n:
+      #print("Nem todas as requisições foram atendidas.")
+      return False
+   
+  if len(req_atendidas) != len(set(req_atendidas)):
+      #print("Requisições repetidas.")
+      return False
+
   return True
 
 def conservacao_de_fluxo(solucao: Solucao, dados: Dados):
@@ -45,12 +52,13 @@ def janela_de_tempo_da_coleta(solucao: Solucao, dados: Dados):
       for index, req in enumerate(rota[1:-1]): 
         inicio = dados.e[req-1]
         fim = dados.l[req-1]
-        tempo_chegada = solucao.chegada[k][v][index+1]
+        tempo_chegada = solucao.chegada[k][v][index]
         if tempo_chegada < inicio or tempo_chegada > fim:
           return False
   return True
 
 def sequencia_temporal_das_rotas_intra(solucao: Solucao, dados: Dados):
+  #Garante a consistência dos tempos de chegada entre requisições consecutivas de uma viagem v (M é uma constante grande).
   # solucao.chegada[k][v][i] < solucao.chegada[k][v][i+1]
   for k, viagens in solucao.rota.items():
       for v, rota in viagens.items():
@@ -64,6 +72,7 @@ def sequencia_temporal_das_rotas_intra(solucao: Solucao, dados: Dados):
   return True
 
 def Sequencia_temporal_das_rotas_inter(solucao: Solucao, dados: Dados):
+  #A partida da garagem para uma nova viagem v só pode ocorrer após o retorno do ônibus k da viagem anterior v −1.
   # solucao.chegada[k][v][0] <= solucao.chegada[k][v-1][-1]
   for k, viagens in solucao.rota.items():
       for v, rota in viagens.items():
@@ -92,23 +101,23 @@ def limite_de_tempo_por_viagem(solucao: Solucao, dados: Dados) -> bool:
   return True
 
 def eh_factivel(solucao: Solucao, dados: Dados) -> bool:
-   factivel = True
+   
    if not(atendimento_requisicoes(solucao, dados)):
       print("Restrição 'atendimento_requisicoes' não atendida.")
-      factivel = False
+      return False
    if not(inicio_e_fim_de_cada_viagem(solucao, dados)):
       print("Restricao 'inicio_e_fim_de_cada_viagem' não atendida.")
-      factivel = False
+      return False
    if not(janela_de_tempo_da_coleta(solucao, dados)):
       print("Restricao 'janela_de_tempo_da_coleta' não atendida.")
-      factivel = False
+      return False
    if not(sequencia_temporal_das_rotas_intra(solucao, dados)):
       print("Restricao 'sequencia_temporal_das_rotas_intra' não atendida.")
-      factivel = False
+      return False
    if not(Sequencia_temporal_das_rotas_inter(solucao, dados)):
       print("Restricao 'Sequencia_temporal_das_rotas_inter' não atendida.")
-      factivel = False
+      return False
    if not(limite_de_tempo_por_viagem(solucao, dados)):
       print("Restricao 'limite_de_tempo_por_viagem' não atendida.")
-      factivel = False
-   return factivel
+      return False
+   return True
